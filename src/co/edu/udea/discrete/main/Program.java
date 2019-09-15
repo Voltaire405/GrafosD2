371a0e459bba41c10d829cb07a26e19df56a6cd0
Voltaire405 <jlma.physics@gmail.com>
2019-09-15 15:27:23 -0500

    menu y clases

--- src/co/edu/udea/discrete/main/Program.java
+++ src/co/edu/udea/discrete/main/Program.java
@@ -1,7 +1,59 @@
 package co.edu.udea.discrete.main;
 
+import co.edu.udea.discretes.model.Graph;
+import jdk.nashorn.internal.runtime.ParserException;
+
+import javax.swing.*;
+import java.text.ParseException;
+
 public class Program {
     public static void main(String[] args) {
-        
+        Graph graph;
+        String userEntry;
+        int entryVertexConnections, opt = 0;
+        int vertexNum = 0;
+        int sides;
+
+        try {
+            opt = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccione el tipo de grafo: \n1. Dirigido.\n2.No dirigido.\n0.Salir", "0"));
+            if (opt != 1 || opt != 2) {
+                System.exit(0);
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Entrada no válida.");
+        }
+
+
+        userEntry = JOptionPane.showInputDialog(null, "Ingrese por favor las aristas de su Grafo separado por " +
+                "','. Ej. 14,25,13,32,... empleando números enteros para identificar los vertices y sin dejar espacios.");
+        userEntry = userEntry.replace(",", "");
+
+        //User Validations.
+        try {
+            int integer = 0;
+            for (char c : userEntry.toCharArray()) {
+                integer = Integer.parseInt(c + "");
+                if (integer < 1) {
+                    throw new Exception("Identificador de Vertices no puede, ser menor a 1");
+                }
+                if (integer > vertexNum) {
+                    vertexNum = integer;
+                }
+                System.out.println(integer);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+        entryVertexConnections = userEntry.length();
+        if (entryVertexConnections % 2 != 0) {
+            JOptionPane.showMessageDialog(null, "Formato de ingreso del grafo no válido");
+        } else {
+            //JOptionPane.showMessageDialog(null,"Formato de ingreso del grafo válido");
+        }
+
+        //instantiate Graph class.
+        graph = new Graph(vertexNum, userEntry, opt);
+
     }
 }
--- src/co/edu/udea/discretes/model/Graph.java
+++ src/co/edu/udea/discretes/model/Graph.java
@@ -1,11 +1,22 @@
 package co.edu.udea.discretes.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Graph {
-    private int numVertex;
-    private List<Edge> edges;
+    private int numVertex, type;
+    private String representation;
+    private List<List> mAdjacency;
+    private List<List> mIncidence;
+
+    public Graph(int numVertex, String representation, Integer type) {
+        this.numVertex = numVertex;
+        this.representation = representation;
+        this.type = type;
+        calculateAdjacency();
+        calculateIncidence();
+    }
 
     public int getNumVertex() {
         return numVertex;
@@ -13,6 +24,72 @@ public class Graph {
 
     public void setNumVertex(int numVertex) {
         this.numVertex = numVertex;
+    }
+
+    public String getRepresentation() {
+        return representation;
+    }
+
+    public void setRepresentation(String representation) {
+        this.representation = representation;
+    }
+
+    public void calculateAdjacency() {
+        //Asigna la matriz de adyacencia, los indices cero se omiten aunque aparezcan por prácticidad.
+        String[] characters = representation.split("");
+        this.mAdjacency = new ArrayList<>(this.numVertex);//Rows
+        for (int i = 0; i <= numVertex; i++) {
+            mAdjacency.add(new ArrayList<Integer>(Collections.nCopies(numVertex + 1, 0)));     //columns
+
+        }
+
+        for (int k = 0; k < representation.length(); k += 2) {
+            int h = k + 1;
+            mAdjacency.get(Integer.parseInt(characters[k])).remove(Integer.parseInt(characters[h]));
+            mAdjacency.get(Integer.parseInt(characters[k])).add(Integer.parseInt(characters[h]), 1);
+
+        }
+
+    }
+
+    public void calculateIncidence() {
+        String[] characters = representation.split("");
+        int j;//aux. var.
+        boolean bucle;
+
+        this.mIncidence = new ArrayList<>(this.representation.length() / 2 + 1);//Rows
+        for (int i = 0; i <= this.representation.length() / 2; i++) {
+            mIncidence.add(new ArrayList<Integer>(Collections.nCopies(numVertex + 1, 0)));     //columns
+
+        }
+
+        for (int h = 1; h < mIncidence.size(); h++) {
+            j = 0;
+            for (int k = 2 * (h - 1); j < 2; k++) {
+                j++;
+                if ((k + 1) % 2 == 0) {
+                    bucle = Integer.parseInt(characters[k]) == Integer.parseInt(characters[k - 1]);
+                    mIncidence.get(h).remove(Integer.parseInt(characters[k]));
+                    mIncidence.get(h).add(Integer.parseInt(characters[k]), bucle ? 2 : -1);
+
+                } else {
+                    mIncidence.get(h).remove(Integer.parseInt(characters[k]));
+                    mIncidence.get(h).add(Integer.parseInt(characters[k]), 1);
+                }
+            }
+        }
+    }
+
+    public Boolean isComplete() {
+        return true;
+    }
+
+    public Boolean isRegular() {
+        return true;
+    }
+
+    public Boolean isEulerian() {
+        return true;
     }
 
 }
